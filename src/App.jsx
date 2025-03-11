e00b8160c2df4caaea928dd8f3fc10bb8090b9ac
abdullah2004-kalvium <abdullahm6270@gmail.com>
2025-03-11 10:23:21 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BookCard from './components/bookcard';
 
 function App() {
   const books = [
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,18 @@
-// write the book component code here
+import React from "react";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4 w-64 text-center">
+      <img
+        src={book.image}
+        alt={book.title}
+        className="w-full h-40 object-cover rounded-md"
+      />
+      <h2 className="text-lg font-semibold mt-2">{book.name}</h2>
+      <p className="text-black text-sm">{book.genre}</p>
+      <p className="text-gray-600 text-sm">Author: {book.author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
